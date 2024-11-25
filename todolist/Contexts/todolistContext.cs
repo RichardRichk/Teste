5dd353b1c1ad99c1811d3d854c67b90d36db8b7b
Richard Passarelli <richardfpassarelli@gmail.com>
2024-11-25 18:01:53 -0300

    estilização

--- todolist/Contexts/todolistContext.cs
+++ todolist/Contexts/todolistContext.cs
@@ -28,6 +28,7 @@ public partial class todolistContext : DbContext
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Data Source=NOTE17-S21\\SQLSERVER; initial catalog=todolist; User Id = sa; Pwd = Senai@134; TrustServerCertificate=true;");
 
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Prioridade>(entity =>
